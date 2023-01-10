7d24dd8ded24c3fd6a390b10f83183dc41ed3ac6
kumar2014 <kumaravel.au@gmail.com>
2023-01-10 15:24:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-template-reference
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-8hsjsi)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, DemoComponent, CustomerListComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
